466c9d619e8a814e9765a9cda8f677bbe8e1af79
SGS-J <santigarsan07@gmail.com>
2022-10-01 18:21:16 -0500

    Initial setup by spring initializr

---
+++ src/main/java/com/SGSJ/Saturn/SaturnSystemApplication.java
@@ -0,0 +1,13 @@
+package com.SGSJ.Saturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturnSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaturnSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SGSJ/Saturn/SaturnSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SGSJ.Saturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaturnSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
