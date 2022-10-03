1002b105d9ee1596efa28a8444d26b270020eddb
SGS-J <santigarsan07@gmail.com>
2022-10-03 10:39:05 -0500

    Setup Javafx with spring boot

--- src/main/java/com/SGSJ/Saturn/SaturnSystemApplication.java
+++ src/main/java/com/SGSJ/Saturn/SaturnSystemApplication.java
@@ -3,9 +3,9 @@ package com.SGSJ.Saturn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SaturnSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SaturnSystemApplication.class, args);
 	}
---
+++ src/main/java/com/SGSJ/Saturn/SaturnUIApplication.java
@@ -0,0 +1,11 @@
+package com.SGSJ.Saturn;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SaturnUIApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
