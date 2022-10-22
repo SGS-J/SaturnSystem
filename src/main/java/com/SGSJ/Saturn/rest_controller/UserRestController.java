6cb2a062994376b190f672cc14ccec57b13b81ec
SGS-J <santigarsan07@gmail.com>
2022-10-21 22:01:31 -0500

    Configure Cors for application. Add build jar plugins and scripts

--- src/main/java/com/SGSJ/Saturn/rest_controller/UserRestController.java
+++ src/main/java/com/SGSJ/Saturn/rest_controller/UserRestController.java
@@ -6,12 +6,14 @@ import com.SGSJ.Saturn.exceptions.InvalidFileTypeException;
 import com.SGSJ.Saturn.exceptions.InvalidLogInException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
---
+++ src/main/java/com/SGSJ/Saturn/rest_controller/WebConfig.java
@@ -0,0 +1,21 @@
+package com.SGSJ.Saturn.rest_controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Configures CORS
+// Configuramos CORS
+@Controller
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${spring.application.domain.cors}")
+    private String corsURL;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(corsURL);
+    }
+}
